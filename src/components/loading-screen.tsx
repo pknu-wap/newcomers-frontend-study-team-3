f66c0b34e5a490db8d4fd7aa057c7096820320fb
mututu17 <161244328+mututu17@users.noreply.github.com>
2026-02-27 01:56:31 +0900

    feat: #2 SET UP 끝
    라우팅, 로딩 스크린, 파이어베이스 설정, git ignore 에 .env 추가

---
+++ src/components/loading-screen.tsx
@@ -0,0 +1,14 @@
+import {styled} from "styled-components";
+
+const Wrapper = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const Text = styled.span``;
+
+export default function LoadingScreen() {
+    return <Wrapper><Text>Loading...</Text></Wrapper>
+}
