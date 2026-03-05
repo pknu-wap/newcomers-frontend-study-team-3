54cf7bda84174b0014982667c80b4415a2736750
mututu17 <161244328+mututu17@users.noreply.github.com>
2026-03-05 20:32:02 +0900

    feat: login 페이지 구현
    create-account 페이지를 재사용하고
    컴포넌트를 분리하여 login을 페이지를 구현했습니다.

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,44 +1,10 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import styled from "styled-components";
 import { auth } from "./firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-const Title = styled.h1`
-    font-size: 42px;
-`;
-const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;`;  
-const Form = styled.form`
-    margin-top: 50px;
-    margin-bottom: 10px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-    `;
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit"]{
-     cursor: pointer;
-     &:hover{
-     opacity: 0.8;
-    }
-    }
-`;
-const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
-`;
+import { Form, Input, Swithcher, Title, Wrapper, Error } from "../components/auth-components";
+
 export default function CreateAccount() {  
     const navigate = useNavigate();
     const [isLoading, setLoading] = useState(false);
@@ -90,5 +56,9 @@ export default function CreateAccount() {
             <Input type ="submit" value={isLoading ? "Loading..." : "Create Account"}></Input>
         </Form>
         {error !== "" ? <Error>{error}</Error>: null}
+        <Swithcher>
+                Already have an account?{""}
+                <Link to="/login">Log in &rarr;</Link>
+        </Swithcher>
     </Wrapper>;
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,58 @@
-export default function Login() {   
-    return <h1>Login</h1>;
+import { useState } from "react";
+import { auth } from "./firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Form, Input, Swithcher, Title, Wrapper, Error } from "../components/auth-components";
+
+export default function Login() {  
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {target: {name, value}} = e;
+        if(name === "email") {
+            setEmail(value);
+        } else if(name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        if(isLoading || email === "" || password === "") return;
+        try{
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+             // create an account
+            // set the name of the user
+            // redirect to the home page
+        } catch(e){
+            if(e instanceof FirebaseError)
+            {
+                setError(e.message);
+            }
+         }
+        finally{
+            setLoading(false);
+        }
+       
+        console.log(name, email, password);
+    }
+    return <Wrapper>
+        <Title>Log into 𝕏</Title>
+        <Form onSubmit={onSubmit}>
+            <Input onChange={onChange} name ="email" value={email} placeholder="Email" type="email" required></Input>
+            <Input onChange={onChange} name ="password" value={password} placeholder="Password" type="password" required></Input>
+            <Input type ="submit" value={isLoading ? "Loading..." : "Log in"}></Input>
+        </Form>
+        {error !== "" ? <Error>{error}</Error>: null}
+        <Swithcher>
+            Don't have an account?{""}
+            <Link to="/create-account">Create one &rarr;</Link>
+        </Swithcher>
+    </Wrapper>;
 }
