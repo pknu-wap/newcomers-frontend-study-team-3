3930fe91fb65073936bb07121f5b1d957e0b530f
mututu17 <161244328+mututu17@users.noreply.github.com>
2026-03-14 00:18:23 +0900

    feat: 프로필 구현

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,65 @@
+import { useState } from "react";
+import { auth } from "./firebase";
+import styled from "styled-components";
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+`;
+const AvatarUpload = styled.label`
+    width: 80px;
+    overflow: hidden;
+    height: 80px;
+    border-radius: 50%;
+    background-color: #1d9bf0;
+    cursor: pointer;
+    distplay: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+        width: 50px;
+    }
+`;
+const AvatarImg = styled.img`
+    width: 100%;
+`;
+const AvatarInput = styled.input`
+    display: none;
+`;
+const Name = styled.span`
+    font-size: 22px;
+`;
+
+
+
+
 export default function Profile() {
-    return <h1>Profile</h1>;
+    const user = auth.currentUser;
+    const [avatar, setAvatar] = useState(user?.photoURL);
+    const onAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {files} = e.target;
+        if(!user) return;
+        if(files && files.length === 1){
+            const file = files[0];
+        }
+        //아바타 업데이트 로직 구현하기
+    };
+    return <Wrapper>
+         <AvatarUpload htmlFor="avatar">
+            {avatar ? <AvatarImg src={avatar}/> : 
+                <svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z"></path>
+</svg>
+            }
+         </AvatarUpload>
+         <AvatarInput
+         onChange={onAvatarChange} 
+         id = "avatar" 
+         type="file" 
+         accept="image/*"/>
+         <Name>
+            {user?.displayName ?? "Anonymous"}
+         </Name>
+        </Wrapper>;
 }
