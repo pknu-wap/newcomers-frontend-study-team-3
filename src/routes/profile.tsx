b95b923e34117087058acc13a920ebc4ed4e03dc
kjm070909 <kjm070909@naver.com>
2026-03-13 22:36:37 +0900

    프로필 사진 업로드 기능 구현
    프로필 사진 업로드 기능 구현

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,6 +1,70 @@
+import styled from "styled-components";
+import { auth, storage } from "../firebase"
+import { useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
+
 export default function profile() {
+    const user = auth.currentUser
+    const [avatar, setAvatar] = useState<string | null>(user?.photoURL || null)
+    const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { file } = e.target.files?.[0]
+        if (!user) return;
+        if (file && FileSystem.length === 1) {
+          const file = file[0];
+          const locationRef = ref(storage, `avatars/${user?.uid}`);
+          const result = await uploadBytes(locationRef, file);
+          const url = await getDownloadURL(locationRef);
+          setAvatar(url);
+          await updateProfile(user, {
+            photoURL: url,
+          })
+        }
+      }
     return (
-        <h1>profilewwww</h1>
+        <Wrapper>
+            <AvatarUpload htmlFor="avatar">
+              {Boolean(avatar) ? <AvatarImg src={avatar} alt="avatar" /> : null}
+                <AvatarImg/> 
+            </AvatarUpload>
+            <AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*"/>
+            <Name>
+                {user?.displayName ? user.displayName : "No name"}
+
+            </Name>
+        </Wrapper>
 
     )
 }
